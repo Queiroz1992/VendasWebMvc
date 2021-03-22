a9a525fe963f999fd1c79fcc37f7a0f508952fb3
Filipe Queiroz <filipequeiroz@outlook.com>
2021-03-22 17:46:42 -0300

    ServicoVendedor e Método para ObterTodosVendedores da lista

--- VendasWebMvc/Controllers/VendedoresController.cs
+++ VendasWebMvc/Controllers/VendedoresController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VendasWebMvc.Services;
 
 namespace VendasWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
+        private readonly ServicoVendedor _servicoVendedor;
+
+        public VendedoresController(ServicoVendedor servicoVendedor)
+        {
+            _servicoVendedor = servicoVendedor;                
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _servicoVendedor.ObterTodosVendedores();
+            return View(list);
         }
     }
 }
--- VendasWebMvc/Startup.cs
+++ VendasWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using VendasWebMvc.Data;
+using VendasWebMvc.Services;
 
 namespace VendasWebMvc
 {
@@ -41,6 +42,7 @@ namespace VendasWebMvc
 
             //registro do servico no sistema de injenção de independencia da aplicação
             services.AddScoped<ServicoPopularDados>();
+            services.AddScoped<ServicoVendedor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
